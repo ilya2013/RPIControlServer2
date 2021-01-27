045510460aa1e26452e60a0d66ff13f9c273bac3
iliabezh@gmail.com <iliabezh@gmail.com>
2021-01-27 21:18:18 +0300

    Actuator added Spring version was updated from <version>2.3.3.RELEASE</version> to <version>2.4.2</version>

--- src/main/java/com/ibesh/rpi/kafka/consumer/KafkaConsumerConfig.java
+++ src/main/java/com/ibesh/rpi/kafka/consumer/KafkaConsumerConfig.java
@@ -1,6 +1,5 @@
 package com.ibesh.rpi.kafka.consumer;
 
-import com.ibesh.rpi.dto.DhtDto;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,7 +9,6 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
