9f8768a6f90bb93766d5343b9cf1d9bbf62e5c59
iliabezh@gmail.com <iliabezh@gmail.com>
2021-01-27 22:05:17 +0300

    Log4j added +refactoring

--- src/main/java/com/ibesh/rpi/service/DhtService.java
+++ src/main/java/com/ibesh/rpi/service/DhtService.java
@@ -3,6 +3,7 @@ package com.ibesh.rpi.service;
 import com.ibesh.rpi.dto.DhtDto;
 import com.ibesh.rpi.model.DhtModel;
 import com.ibesh.rpi.repository.DhtRepository;
+import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,30 +12,23 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.format.DateTimeFormatter;
 
 @Component
+@Log4j2
 public class DhtService {
     @Autowired
     DhtRepository dhtRepository;
 
-
-    public void saveDht(DhtModel dhtModel){
-        System.out.println("Start save dht");
-        DhtModel newDhtModel = dhtRepository.save(dhtModel);
-        System.out.println(newDhtModel);
-        System.out.println("Finish save dht");
-        dhtRepository.findAll().forEach(System.out::println);
-    }
     @Transactional
     public void saveDht(DhtDto dhtDto){
-        System.out.println("Start save dht");
+        log.info("Start save dht");
         ModelMapper modelMapper = new ModelMapper();
         DhtModel dhtModel = modelMapper.map(dhtDto, DhtModel.class);
-        System.out.println(dhtModel);
+        log.info(dhtModel);
         // user here is a prepopulated User instance
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH.mm.ss");
         dhtModel.setTime(dhtDto.transformTimeInLocalDateTime(dateTimeFormatter));
         DhtModel newDhtModel = dhtRepository.save(dhtModel);
-        System.out.println(newDhtModel);
-        System.out.println("Finish save dht");
+        log.info(newDhtModel);
+        log.info("Finish save dht");
 //        dhtRepository.findAll().forEach(System.out::println);
     }
 
