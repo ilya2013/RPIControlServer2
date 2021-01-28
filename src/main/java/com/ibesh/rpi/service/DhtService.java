09509ffba4fe2e89be29768e04c0f195b4148239
iliabezh@gmail.com <iliabezh@gmail.com>
2021-01-28 13:29:10 +0300

    Log4j use

--- src/main/java/com/ibesh/rpi/service/DhtService.java
+++ src/main/java/com/ibesh/rpi/service/DhtService.java
@@ -5,6 +5,7 @@ import com.ibesh.rpi.mapper.DHTSourceDestinationMapper;
 import com.ibesh.rpi.model.DhtModel;
 import com.ibesh.rpi.repository.DhtRepository;
 import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
 import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -14,6 +15,7 @@ import java.time.LocalDateTime;
 
 @Component
 @Getter
+@Log4j2
 public class DhtService {
     @Autowired
     DhtRepository dhtRepository;
@@ -21,13 +23,13 @@ public class DhtService {
 
     @Transactional
     public void saveDht(DhtDto dhtDto){
-        System.out.println("Start save dht");
+        log.info("Start save dht");
         DhtModel dhtModel = mapper.sourceToDestination(dhtDto);
-        System.out.println(dhtModel);
+        log.info(dhtModel);
         dhtModel.setReceivedTime(LocalDateTime.now());
         DhtModel newDhtModel = dhtRepository.save(dhtModel);
-        System.out.println(newDhtModel);
-        System.out.println("Finish save dht");
+        log.info(newDhtModel);
+        log.info("Finish save dht");
     }
 
 }
