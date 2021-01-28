00b8a18801bd5da38b5d86a0d9076502ef2b17cd
iliabezh@gmail.com <iliabezh@gmail.com>
2021-01-28 12:37:15 +0300

    Bug "java -jar" run

--- src/main/java/com/ibesh/rpi/kafka/consumer/KafkaConsumer.java
+++ src/main/java/com/ibesh/rpi/kafka/consumer/KafkaConsumer.java
@@ -4,19 +4,19 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ibesh.rpi.dto.DhtDto;
 import com.ibesh.rpi.service.DhtService;
-import lombok.extern.log4j.Log4j2;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Controller;
 
 @Controller
-@Log4j2
+@Log
 public class KafkaConsumer {
     @Autowired
     DhtService dhtService;
     @KafkaListener(topics = "dht", groupId = "foo")
     public void listenGroupFoo(String message) throws JsonProcessingException {
-        log.info("Received dht Message in group foo: {}", message);
+        log.info("Received dht Message in group foo: " + message);
         message = message.replace("\\", "");
         message = message.replace("\"{", "{");
         message = message.replace("}\"", "}");
@@ -25,7 +25,7 @@ public class KafkaConsumer {
             DhtDto dhtDto = mapper.readValue(message, DhtDto.class);
             dhtService.saveDht(dhtDto);
         } catch (Exception e) {
-            log.error(e.getMessage());
+            log.warning(e.getMessage());
         }
     }
 }
--- src/main/java/com/ibesh/rpi/service/DhtService.java
+++ src/main/java/com/ibesh/rpi/service/DhtService.java
@@ -3,7 +3,7 @@ package com.ibesh.rpi.service;
 import com.ibesh.rpi.dto.DhtDto;
 import com.ibesh.rpi.model.DhtModel;
 import com.ibesh.rpi.repository.DhtRepository;
-import lombok.extern.log4j.Log4j2;
+import lombok.extern.java.Log;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,7 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.format.DateTimeFormatter;
 
 @Component
-@Log4j2
+@Log
 public class DhtService {
     @Autowired
     DhtRepository dhtRepository;
@@ -22,12 +22,12 @@ public class DhtService {
         log.info("Start save dht");
         ModelMapper modelMapper = new ModelMapper();
         DhtModel dhtModel = modelMapper.map(dhtDto, DhtModel.class);
-        log.info(dhtModel);
+        log.info(dhtModel.toString());
         // user here is a prepopulated User instance
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH.mm.ss");
         dhtModel.setTime(dhtDto.transformTimeInLocalDateTime(dateTimeFormatter));
         DhtModel newDhtModel = dhtRepository.save(dhtModel);
-        log.info(newDhtModel);
+        log.info(newDhtModel.toString());
         log.info("Finish save dht");
 //        dhtRepository.findAll().forEach(System.out::println);
     }
